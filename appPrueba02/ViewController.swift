cc89ec3bb51a1340767cc0e71f7fb6d63925556e
idiland <idiland@idiland.com>
2022-03-27 15:58:43 +0100

    Initial Commit

---
+++ appPrueba02/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  appPrueba02
+//
+//  Created by user190736 on 3/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var contador = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func perfomVlick(_ sender: UIButton) {
+        contador += 1
+        print(contador)
+        myLabel.text = "Hola Mundo \(contador)"
+    }
+    
+}
+
