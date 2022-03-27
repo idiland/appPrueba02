338f2e08b511a457ad675e8354cb76e164015ae8
idiland <idiland@idiland.com>
2022-03-27 23:01:53 +0100

    Crear View Controller(nuevo) y ver sus estados CiloVida

---
+++ appPrueba02/MiViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MiViewController.swift
+//  appPrueba02
+//
+//  Created by user190736 on 3/27/22.
+//
+
+import UIKit
+
+class MiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
